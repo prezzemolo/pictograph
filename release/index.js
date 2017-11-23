e9495a147d77e118f65eca54ee29f6b93d48a8ea
prezzemolo <git@prezzemolo.ga>
2017-11-23 18:26:17 +0900

    :hocho: add release to .gitignore

--- release/index.js
+++
@@ -1,4 +0,0 @@
-exports.dic = require('./pictograph.js')
-exports.decode = (text) => typeof text === 'string' ? text.replace(/:([a-zA-Z0-9+-_]*?):/g, (match, emoji) => exports.dic[emoji] || match) : text
-exports.version = '61e16ae891b942738a2cf6bda7b3cbe3143d8521'
-exports.hash = 'b23b355776c725f31eb5f42093d6a2b5025ea796'
