f84b90329d662766c5262aa2acaadc52deecb024
prezzemolo <git@prezzemolo.ga>
2017-12-08 22:26:21 +0000

    :gear: tagging: add debug feature

--- .travis/tag.sh
+++ .travis/tag.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
+IFS=',' A_DEBUG=(${DEBUG:-})
+function debug () {
+	local show=false
+	local i
+	for ((i=0; i < ${#A_DEBUG[@]}; i++))
+	do
+		local v=${A_DEBUG[$i]}
+		if [[ $v == '*' ]] || [[ $v == 'pictograph' ]] || [[ $v == 'pictograph:tag' ]]
+		then
+			show=true
+		fi
+	done
+	if [[ $show == true ]]
+	then
+		local now=$(date -uIs)
+		echo "${now:0:-6}Z prezzemolo:tag $@" >&2
+	fi
+}
+
 # get absolute pathes
 SCRIPT=$(readlink -f $BASH_SOURCE)
 DIR=${SCRIPT%/*}
 
 # decrypt deploy key
 dk="$(mktemp)"
+debug key: $dk
 openssl aes-256-cbc \
 	-K $encrypted_a716c936a8d9_key \
 	-iv $encrypted_a716c936a8d9_iv \
@@ -21,28 +41,39 @@ ssh \
 -i $dk
 EOC
 `
+debug com: $GIT_SSH_COMMAND
 
 # run build
 npm run build --silent
 
 # get gemoji commit from latest git tag
 c_tag=$(git describe --tags $(git rev-list --tags --remotes --max-count=1) | cut -d+ -f2)
+debug "c_tag: $c_tag"
 # get gemoji commit from builted
 c_built=$(node -e "console.log(require('$DIR/../release').version)")
+debug c_built: $c_built
 c_built_round=$(node -e "console.log(require('$DIR/../release').version.substr(0, 7))")
+debug c_built_round: $c_built_round
 # get current meta package version
 v_meta=$(node -e "console.log(require('$DIR/../package.json').version)" | cut -d+ -f1)
+debug v_meta: $v_meta
+
 if [ "$c_tag" != "$c_built_round" ]
 then
 	# generate version will be tagged
 	v_release_pre=$(npm run semver --silent -- $v_meta -i patch)
+	debug v_release_pre: $v_release_pre
 	v_release="$v_release_pre+$c_built_round"
+	debug v_release: $v_release
 	# override package.json's version
+	debug override package version
 	node $DIR/override-package-version.js $v_release
 	# stage pacakge.json
+	debug add package.json to stage
 	git add $DIR/../package.json
 	# create commit summary
 	c_file=$(mktemp)
+	debug c_file: $c_file
 	cat > $c_file <<- EOM
 		:package: up to $v_release
 
