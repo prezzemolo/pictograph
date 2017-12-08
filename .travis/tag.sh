72c79895bee44ef935649d414ca9d2e1075955dc
prezzemolo <git@prezzemolo.ga>
2017-12-08 21:33:23 +0000

    :gear: tagging: checking version with git tags
    cuz npm removes build metadata from version.

--- .travis/tag.sh
+++ .travis/tag.sh
@@ -23,17 +23,16 @@ EOC
 `
 
 # run build
-npm run build
+npm run build --silent
 
-# get gemoji commit from current published version
-c_npm=$(npm info pictograph version --silent | cut -d+ -f2)
+# get gemoji commit from latest git tag
+c_tag=$(git describe --tags $(git rev-list --tags --remotes --max-count=1) | cut -d+ -f2)
 # get gemoji commit from builted
 c_built=$(node -e "console.log(require('$DIR/../release').version)")
 c_built_round=$(node -e "console.log(require('$DIR/../release').version.substr(0, 7))")
 # get current meta package version
 v_meta=$(node -e "console.log(require('$DIR/../package.json').version)" | cut -d+ -f1)
-
-if [ "$c_npm" != "$c_built_round" ]
+if [ "$c_tag" != "$c_built_round" ]
 then
 	# generate version will be tagged
 	v_release_pre=$(npm run semver --silent -- $v_meta -i patch)
